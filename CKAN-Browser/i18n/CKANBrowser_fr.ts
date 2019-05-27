cbc561f4632bb5ef9b1b7b7d2cf6abcc53023866
sylvain.pierre <sylvain.pierre@bas-rhin.fr>
2019-05-27 14:01:19 +0200

    French translation

---
+++ CKAN-Browser/i18n/CKANBrowser_fr.ts
@@ -0,0 +1,396 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>CKANBrowserDialogBase</name>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="35"/>
+        <source>dlg_base_title</source>
+        <translation>Explorateur Open Data (CKAN)</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="57"/>
+        <source>dlg_base_search_term</source>
+        <translation>Mot clé:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="90"/>
+        <source>dlg_base_ttip_search</source>
+        <translation>Entrez vos mots clés ici.</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="510"/>
+        <source>?</source>
+        <translation>?</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="102"/>
+        <source>dlg_base_btn_strt_srch</source>
+        <translation>Recherche</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="109"/>
+        <source>dlg_base_ttip_show</source>
+        <translation>Montrer toutes les ressources disponibles sur le serveur CKAN</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="112"/>
+        <source>dlg_base_btn_show_all</source>
+        <translation>Liste tous les jeux de données</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="127"/>
+        <source>dlg_base_filter_to</source>
+        <translation>Limiter la recherche par catégorie:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="168"/>
+        <source>dlg_base_ttip_filter</source>
+        <translation>Activer la case pour limiter la recherche à cette catégorie. Double click sur une catégorie liste tous ses jeux de données.</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="208"/>
+        <source>IDC_lblApiUrl</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="230"/>
+        <source>IDC_lblCacheDir</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="252"/>
+        <source>IDC_lblVersion</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="259"/>
+        <source>dlg_base_btn_disclaimer</source>
+        <translation>Avertissement</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="292"/>
+        <source>dlg_base_srch_rslt</source>
+        <translation type="unfinished">Résultats:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="309"/>
+        <source>IDC_lblPage</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="330"/>
+        <source>dlg_base_ttip_prev</source>
+        <translation>précédent</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="333"/>
+        <source>dlg_base_btn_lt</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="355"/>
+        <source>dlg_base_ttip_next</source>
+        <translation>suivant</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="358"/>
+        <source>dlg_base_btn_gt</source>
+        <translation>&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="382"/>
+        <source>dlg_base_descr</source>
+        <translation>Description:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="401"/>
+        <source>dlg_base_data_list</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Données trouvées:&lt;br /&gt;Choisir le jeu de données à télécharger.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="442"/>
+        <source>dlg_base_ttip_data_list</source>
+        <translation>Télécharge les jeux de données sélectionnés et tente leur ouverture dans QGIS. Si les jeux de données ne peuvent pas s&apos;afficher le répertoire source s&apos;ouvre.</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="466"/>
+        <source>dlg_base_ressource</source>
+        <translation>URL de la ressource:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="507"/>
+        <source>dlg_base_ttip_resource</source>
+        <translation>URL de la resource sélectionnée</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="545"/>
+        <source>dlg_base_ttip_copy</source>
+        <translation>Copier dans le presse-papier</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="578"/>
+        <source>dlg_base_btn_load_data</source>
+        <translation>Charger données</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_base.ui" line="585"/>
+        <source>dlg_base_btn_close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>CKANBrowserDialogSettings</name>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="26"/>
+        <source>dlg_set_title</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="38"/>
+        <source>dlg_set_btn_save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="45"/>
+        <source>dlg_set_btn_cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="75"/>
+        <source>dlg_set_tool_cache</source>
+        <translation>Sélectionner un répertoire dans lequel les données seront téléchargées.</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="220"/>
+        <source>?</source>
+        <translation>?</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="100"/>
+        <source>dlg_set_pre_api</source>
+        <translation>Example OpenData (CKAN) Serveur:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="116"/>
+        <source>dlg_set_cache_path</source>
+        <translation>Répertoire de cache</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="132"/>
+        <source>dlg_set_choose_pre</source>
+        <translation>Appliquer</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="164"/>
+        <source>dlg_set_ckan_api</source>
+        <translation>Serveur  OpenData (CKAN) courant:</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="189"/>
+        <source>dlg_set_tool_pre_urls</source>
+        <translation>URL pré-selectionnées de serveurs CKAN testés</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="217"/>
+        <source>dlg_set_tool_api_url</source>
+        <translation>Sélectionner un serveur OpenData ou saisissez l&apos;URL d&apos;un nouveau serveur.</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="271"/>
+        <source>dlg_set_v3_only</source>
+        <translation>Support V3 uniquement</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="297"/>
+        <source>dlg_set_tool_test</source>
+        <translation>Test de l&apos;URL sélectionnée</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="300"/>
+        <source>dlg_set__btn_test</source>
+        <translation>Test connection</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_settings.ui" line="313"/>
+        <source>dlg_set_ellipsis</source>
+        <translation>...</translation>
+    </message>
+</context>
+<context>
+    <name>CKANBrowserDisclaimer</name>
+    <message>
+        <location filename="../ckan_browser_dialog_disclaimer.ui" line="32"/>
+        <source>dlg_dsc_dlg_title</source>
+        <translation type="unfinished">Avertissement</translation>
+    </message>
+    <message>
+        <location filename="../ckan_browser_dialog_disclaimer.ui" line="70"/>
+        <source>dlg_dsc_title</source>
+        <translation type="unfinished">CKAN Browser</translation>
+    </message>
+</context>
+<context>
+    <name>CKANBrowser</name>
+    <message>
+        <source>&amp;Open Data (CKAN) Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Data (CKAN) Browser</source>
+        <translation type="unfinished">Open Data (CKAN) Browser</translation>
+    </message>
+    <message>
+        <source>ckan_browser_settings</source>
+        <translation>Paramétres</translation>
+    </message>
+    <message>
+        <source>py_dlg_set_warn_urls_not_load</source>
+        <translation type="unfinished">URL prédéfinies ne peuvent pas être chargées: {0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_set_info_conn_succs</source>
+        <translation type="unfinished">Connection réussi</translation>
+    </message>
+    <message>
+        <source>py_dlg_set_warn_cache_not_use</source>
+        <translation type="unfinished">Le répertoire cache ne peut pas être utilisé</translation>
+    </message>
+    <message>
+        <source>py_dlg_set_warn_ckan_url</source>
+        <translation type="unfinished">Définir API CKAN!</translation>
+    </message>
+    <message>
+        <source>dlg_set_tool_cache</source>
+        <translation type="unfinished">Sélectionner un répertoire dans lequel les données seront téléchargées.</translation>
+    </message>
+    <message>
+        <source>dlg_set_tool_pre_urls</source>
+        <translation type="unfinished">Pré-selection URL de serveurs CKAN testés</translation>
+    </message>
+    <message>
+        <source>dlg_set_tool_api_url</source>
+        <translation type="unfinished">Séléctionner un serveur OpenData  ou saisir l&apos;URL d&apos;un nouveau serveur..</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_server</source>
+        <translation>Serveur:&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_cache_path</source>
+        <translation>Répertoire cache:&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_version</source>
+        <translation>Version: {0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_search_result</source>
+        <translation>Résultat de la recherche:</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_page_1_1</source>
+        <translation>Page 1/1</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_search_result_0</source>
+        <translation>Résultats recherche: 0</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_no_result</source>
+        <translation>Pas de résultat</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_result_count</source>
+        <translation>Résultat recherche: {0} jeux de données</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_page_count</source>
+        <translation>Page {0}/{1:.0f}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_warn_no_resource</source>
+        <translation>Aucune ressource sélectionnée</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_warn_no_resource_name</source>
+        <translation type="unfinished">Resource ne peut pas être chargée.\nResource sans nom.\nID: {0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_warn_cache_dir_not_created</source>
+        <translation>Création répertoire cache impossible: {0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_data_already_loaded</source>
+        <translation>Jeu de données déjà téléchargé. Recommencer?</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_download_error</source>
+        <translation type="unfinished">Erreur téléchargement: {0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_big_file</source>
+        <translation>Taille du fichier {0} MB. Télécharger quand même?</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_warn_not_extracted</source>
+        <translation>Extraction archive impossible:&lt;br /&gt;{0}</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_open_manager</source>
+        <translation>Type de fichier inconnu. &lt;br /&gt;&lt;br /&gt; Téléchargé de: &lt;br /&gt;{0} &lt;br /&gt;&lt;br /&gt; Ouvrir répertoire?&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <source>py_dlg_base_lyr_not_loaded</source>
+        <translation>Chargement couche impossible:&lt;br /&gt;{0}&lt;br /&gt;{1}</translation>
+    </message>
+    <message>
+        <source>dlg_base_ttip_search</source>
+        <translation>Entrez vos recherches ici:</translation>
+    </message>
+    <message>
+        <source>dlg_base_ttip_filter</source>
+        <translation>Activez la case à cocher pour limiter la recherche à cette catégorie. Double-click sur une catégorie pour lister tous ses jeux de données.</translation>
+    </message>
+    <message>
+        <source>dlg_base_ttip_data_list</source>
+        <translation>Télécharge les jeux de données sélectionnés et tente de l&apos;ouvrir dans QGIS. Si les jeux de données ne s&apos;affichent pas, le répertoire est ouvert.</translation>
+    </message>
+    <message>
+        <source>dlg_base_ttip_resource</source>
+        <translation>URL de la resource sélectionnée</translation>
+    </message>
+    <message>
+        <source>cc_url_error</source>
+        <translation type="unfinished">Erreurr URL&lt;br /&gt; {0} &lt;br /&gt;&lt;br /&gt;{1}</translation>
+    </message>
+    <message>
+        <source>cc_download_error</source>
+        <translation type="unfinished">Erreur de téléchargement: {0}</translation>
+    </message>
+    <message>
+        <source>cc_connection_timeout</source>
+        <translation>Connection Timeout:&lt;br /&gt;&lt;br /&gt;{0}</translation>
+    </message>
+    <message>
+        <source>cc_api_not_accessible</source>
+        <translation type="unfinished">API CKAN inaccessible</translation>
+    </message>
+    <message>
+        <source>cc_server_fault</source>
+        <translation type="unfinished">Serveur:&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <source>cc_invalid_json</source>
+        <translation type="unfinished">Réponse serveur objet JSON invalide</translation>
+    </message>
+    <message>
+        <source>cc_wrong_api</source>
+        <translation type="unfinished">Support V3 uniquement</translation>
+    </message>
+    <message>
+        <source>py_disc_info_html</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;p, li { white-space: pre-wrap; }&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;.Helvetica Neue DeskInterface&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;&lt;h1&gt;Disclaimer&lt;/h1&gt;&lt;p&gt;EN Es besteht die Möglichkeit, dass das Plugin nicht mit allen CKAN funktioniert, da die Metadaten der Plattformen unterschiedlich abgebildet werden. Unterstützt werden nur Server, die groups oder tags verwenden. &lt;a href=&quot;http://docs.ckan.org/en/latest/api/&quot;&gt;link auf CKAN docs&lt;/a&gt;&lt;br /&gt;Dieses Plugin funktioniert nur mit API-Verson 3&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
